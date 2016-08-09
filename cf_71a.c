fbf34638f7927247a1d600d5665285d9d13fda16
YuXun LU <yuxun.lu.cn@ieee.org>
2016-08-10 05:59:03 +0900

    cf71a clear

---
+++ cf_71a.c
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <cstring>
+using namespace std;
+int main()
+{
+	int n = 0;
+	char str_word[100] = {0};
+	scanf("%d",&n);
+	for(int i = 0; i < n; i++)
+	{
+		scanf("%s", str_word);
+		int l = strlen(str_word);
+		if (l > 10) //over 10 characters
+		{
+			printf("%c%d%c\n", str_word[0], l - 2, str_word[l - 1]);
+		}
+		else
+		{
+			printf("%s\n", str_word);
+		}
+	}
+	return 0;
+}
