700f4c63598921841753262c454f631f93527d74
YuXun LU <yuxun.lu.cn@ieee.org>
2016-08-14 07:59:11 +0900

    Nearly done. BFS one bug fixed

--- poj1753.cpp
+++ poj1753.cpp
@@ -26,12 +26,10 @@ unsigned long int changeState(unsigned long int state, unsigned char bit_flag)
 	unsigned long int res = state;
 	if (isBlack(state, bit_flag) )
 	{
-		//change to white
 		res = res ^ BIT_OPR[bit_flag];
 	}
 	else
 	{
-		//change to black
 		res = res | BIT_OPR[bit_flag];
 	}
 	return res;
@@ -42,49 +40,43 @@ int bit_search(unsigned long int init){
 	unsigned long int now = init;
 	unsigned long int state_queue[MAX_STATE] = {0};
 	unsigned int ind = 0, tail = 0;
+
+	state_queue[ind % MAX_STATE] = init;
 	state[init] = 1;
-	if ( now == 0 || now == 65535 ){
-		ans = 0;
-		return ans;
-	}
-	else{
-		state_queue[ind % MAX_STATE] = init;
-		ind++;
-	}
-//	long int step = 0;
+	ind++;
 	while ((ind % MAX_STATE) != (tail % MAX_STATE) ) {
 		now = state_queue[tail % MAX_STATE];
-		state[now] = 1; //record the state
 		if (now == 0 || now == 65535){
+//			printf("Found %lu, at step %d\n", now, step[now]);
 			ans = step[now];
-			break;
+			return ans + 1;
 		}
 		tail++;
 		for(int i = 0; i < 16; i++)	{
 			unsigned long int new_state = 0;
-			//state expands for 16 different points.
-				/* condition 1: 0 <= i < 4 -> Line 1
-				   condition 2: 4 <= i < 8 -> Line 2
-				   condition 3: 8 <= i < 12 -> Line 3
-				   condition 4: 12 <= i < 16 -> Line 4
-				   i = 0, 3, 12, 15 > corner
-				   i = 4, 8  > left side
-				   i = 7, 11 > right side
-				*/
+			//expand state for 16 points
 			new_state = changeState(now, i);
-			if ( i - 1 > (i / 4) * 4 )	//left
+			if ( i - 1 >= (i / 4) * 4 )	//left
+			{
 				new_state = changeState(new_state, i - 1);
-			if ( i + 1 <= (i / 4 + 1) * 4) //right
+			}
+			if ( i + 1 < (i / 4 + 1) * 4) //right
+			{
 				new_state = changeState(new_state, i + 1);
-			if ( (((i / 4) - 1 * 4) + i % 4) > 0) // up
+			}
+			if ( ((((i / 4) - 1) * 4) + i % 4) >= 0) // up
+			{
 				new_state = changeState(new_state, (i / 4 - 1) * 4 + i % 4 );
-			if ( (((i / 4) + 1 * 4) + i % 4) < 16) //down
+			}
+			if ( ((((i / 4) + 1 ) * 4) + i % 4) < 16) //down
+			{
 				new_state = changeState(new_state, (i / 4 + 1 ) * 4 + i % 4);
-//			printf("Iter %d: Current state %lu, new state %lu, ind %d, tail %d\n", i, now, new_state, ind, tail);
+			}
 			if (state[new_state] != 1) //never expanded before
 			{
 				step[new_state] = step[now] + 1;
 				state_queue[ind % MAX_STATE] = new_state;
+				state[new_state] = 1;
 				ind++;
 			}
 		}
@@ -92,17 +84,13 @@ int bit_search(unsigned long int init){
 	return ans;
 }
 int main(){
-//init bit opr
 	for(int i = 0; i < 16; i++){
 		BIT_OPR[i] = 1 << (15 - i);
-//		printf("Ind:%d, BIT_OPR%lu\n",i,BIT_OPR[i]);
 	}
-//	printf("%d". MAX_STATE);
 	char current_state_input[4] = {0};
 	unsigned long int current_state = 0;
 	for (int i = 0; i < 4; i++){
 		scanf("%s\n",current_state_input);
-//		printf("str len %ld\n", strlen(current_state_input));
 		for (int j = 0; j < strlen(current_state_input); j++){
 			if (current_state_input[j] == 'b' ){
 				current_state = current_state | 1;
